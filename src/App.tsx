d3bc07e02cc854967c3cf8d1fcd2f6c072359405
Gerald Santos <79993851+Gerald-0@users.noreply.github.com>
2022-10-12 12:34:28 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Container from './templates/Container'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {/* <div
+          style={{
+            width: '290px',
+            height: '100vh',
+            backgroundColor: 'grey',
+            float: 'left'
+          }}
+        >
+          <h1>sou uma sidebar fake</h1>
+        </div> */}
+        <Container />
+      </div>
+    </div>
+  )
+}
+
+export default App
